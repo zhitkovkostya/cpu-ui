bad4788e46470e346cf1199574e45bf5cc90efb5
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2019-11-14 23:41:29 +0300

    add grid

---
+++ stories/grid.stories.js
@@ -0,0 +1,45 @@
+export default {
+  title: 'Grid'
+}
+
+export const basic = () => `
+    <div class="grid">
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>  
+    </div>
+`
+
+export const bordered = () => `
+    <div class="grid grid--bordered">
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>
+        <div class="grid__cell"></div>  
+    </div>
+`
