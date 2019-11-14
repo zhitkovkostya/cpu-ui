0b1c01056495fc94f4ded692f8629723ad97ba5d
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2019-11-14 23:17:21 +0300

    add text color modifiers

---
+++ stories/text.stories.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Text'
+}
+
+export const basic = () => `
+    <div class="text">text</div>
+`
+
+export const colored = () => `
+    <div class="text">text</div>
+    <div class="text text--gray-4">text</div>
+    <div class="text text--white">text</div>
+`
