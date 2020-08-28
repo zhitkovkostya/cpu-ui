ab10c1735e7b8cd361499b5ba504e791596d7bd3
Konstantin Zhitkov <KZhitkov@get-bc.local>
2020-08-28 11:49:38 +0300

    Align scripts to the standard code style

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,35 +1,31 @@
-window.addEventListener('scroll', debounce(onScroll));
+window.addEventListener('scroll', debounce(onScroll))
 
-function debounce(func, wait) {
-  var wait = wait || 100,
-    timeout;
+function debounce (func, _wait) {
+  var wait = _wait || 100
+  var timeout
 
-  return function() {
-    clearTimeout(timeout);
+  return function () {
+    clearTimeout(timeout)
     timeout = setTimeout(() => {
-      func.apply(this);
-    }, wait);
-  };
+      func.apply(this)
+    }, wait)
+  }
 }
 
-function onScroll() {
-  var sectionElements = document.querySelectorAll('.section'),
-    hash;
+function onScroll () {
+  var sectionElements = document.querySelectorAll('.section')
 
   sectionElements.forEach(function (sectionElement, index) {
-    var sectionId = index > 0 ? sectionElement.id : '',
-      sectionRect = sectionElement.getBoundingClientRect(),
-      hash = '#' + sectionId;
-
-    console.log(sectionElement.id, sectionRect.top, sectionRect.height / 3);
+    var sectionId = index > 0 ? sectionElement.id : ''
+    var sectionRect = sectionElement.getBoundingClientRect()
+    var hash = '#' + sectionId
 
     if (Math.abs(sectionRect.top) < sectionRect.height / 3) {
-      if (history.pushState) {
-        history.pushState(null, null, hash);
+      if (window.history.pushState) {
+        window.history.pushState(null, null, hash)
       } else {
-        location.hash = hash;
+        window.location.hash = hash
       }
     }
-  });
-  console.log('---');
+  })
 }
