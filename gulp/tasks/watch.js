cdab82c251eedbe9176d47cd7cc8bcff5e096a19
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2023-01-05 21:22:10 +0300

    fix(gulp): update dev command

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,5 +1,5 @@
 const { series, watch } = require("gulp");
-const { sourceFiles } = require("../config");
+const { sourcePaths } = require("../config");
 const compileJs = require("./compile-js");
 const compileTs = require("./compile-ts");
 const compileScss = require("./compile-scss");
@@ -9,8 +9,8 @@ const cleanJs = require("./clean-js");
  * Runs in background waiting for file changes to run specific tasks
  */
 const watchers = () => {
-  watch(sourceFiles.allStyles, compileScss);
-  watch(sourceFiles.allScripts, series(compileTs, compileJs, cleanJs));
+  watch(sourcePaths.allStyles, compileScss);
+  watch(sourcePaths.allScripts, series(compileTs, compileJs, cleanJs));
 };
 
 module.exports = watchers;
